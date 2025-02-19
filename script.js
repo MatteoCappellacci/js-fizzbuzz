80476dcafa0e8f6ea360541d1ab65a0cd8f4851b
MatteoCappellacci <Matteo.cappellacci1987@gmail.com>
2025-02-19 12:50:07 +0100

    Feat: completo

--- script.js
+++ script.js
@@ -18,14 +18,15 @@ PROCEDIMENTO LOGICO
 
 
 
-for (let i = 0; i <= 100; i++) { 
+for (let i = 1; i <= 100; i++) { 
     let fizzBuzz;
     if ( i % 3 === 0 && i % 5 === 0 )  {fizzBuzz = "fizzBuzz"}
     else if ( i % 3 === 0) { fizzBuzz = "fizz"}
     else if ( i % 5 === 0 ) { fizzBuzz = "Buzz"}
+    else { fizzBuzz = i}
     
     
-    console.log(i,fizzBuzz);
+    console.log(fizzBuzz);
 
    
 }
